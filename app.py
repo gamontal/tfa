3ab97cf8fbecefa0c9d92fd45ab8ec779d98a638
Gabriel Montalvo <gabe.mon5@outlook.com>
2016-06-01 14:44:09 -0400

    minor changes

--- app.py
+++ app.py
@@ -5,18 +5,18 @@ import os
 from flask import Flask, jsonify, make_response, request, redirect, url_for, send_from_directory
 from tfa import *
 
-server = Flask(__name__)
+app = Flask(__name__)
 
-@server.route('/')
+@app.route('/')
 def root():
-    return server.send_static_file('index.html')
+    return app.send_static_file('index.html')
 
-@server.route('/<path:path>')
+@app.route('/<path:path>')
 def static_proxy(path):
   # send_static_file will guess the correct MIME type
-  return server.send_static_file(path)
+  return app.send_static_file(path)
 
-@server.route('/tfa', methods=['POST'])
+@app.route('/tfa', methods=['POST'])
 def get_results():
 
     inputfile = request.files['inputfile']
@@ -34,13 +34,13 @@ def get_results():
 
     return jsonify(results), 200
 
-@server.errorhandler(404)
+@app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({ 'error': 'Not Found' }), 404)
 
-@server.errorhandler(400)
+@app.errorhandler(400)
 def bad_request(error):
     return make_response(jsonify({ 'error': 'Bad Request'}), 400)
 
 if __name__ == '__main__':
-    server.run(host='0.0.0.0', port=8080) # start flask server on port 8080
+    app.run(host='0.0.0.0', port=8080) # start flask server on port 8080
